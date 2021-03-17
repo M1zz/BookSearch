fc7be980dcd10b9e9a7e21b10ba4afe5fcd08592
hyunho.lee <mizzking75@gmail.com>
2021-03-18 00:56:58 +0900

    Add SearchViewController with navigationController

---
+++ BookSearch/Screens/SearchViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SearchViewController.swift
+//  BookSearch
+//
+//  Created by 이현호 on 2021/03/15.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        title = "검색"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
