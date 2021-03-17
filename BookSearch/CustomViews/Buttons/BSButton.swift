de2a39ceab598c86f4c50200ef5fdd9a52519d2f
hyunho.lee <mizzking75@gmail.com>
2021-03-18 08:14:06 +0900

    Add CustomButton

---
+++ BookSearch/CustomViews/Buttons/BSButton.swift
@@ -0,0 +1,37 @@
+//
+//  BSButton.swift
+//  BookSearch
+//
+//  Created by 이현호 on 2021/03/18.
+//
+
+import UIKit
+
+class BSButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
