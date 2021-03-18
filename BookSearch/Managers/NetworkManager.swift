dc77dd06090a26783a12ce25dc6ad0f68efb40b5
hyunho.lee <mizzking75@gmail.com>
2021-03-18 22:29:42 +0900

    Add image cache

--- BookSearch/Managers/NetworkManager.swift
+++ BookSearch/Managers/NetworkManager.swift
@@ -10,6 +10,7 @@ import UIKit
 class NetworkManager {
     static let shared = NetworkManager()
     let baseURL = "https://api.itbook.store/1.0/"
+    let cache = NSCache<NSString, UIImage>()
     // search https://api.itbook.store/1.0/search/{query}/{page}
     // detail https://api.itbook.store/1.0/books/{isbn13}
     
@@ -54,13 +55,20 @@ class NetworkManager {
     
     
     func downloadImage(from urlString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
         guard let url = URL(string: urlString) else {
             completed(nil)
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            guard let _ = self,
+            guard let self = self,
                   error == nil,
                   let response = response as? HTTPURLResponse, response.statusCode == 200,
                   let data = data,
@@ -68,6 +76,9 @@ class NetworkManager {
                 completed(nil)
                 return
             }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            
             completed(image)
         }
         
