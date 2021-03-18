9f38f49356dfd6cff5f742d1510261240d0e8dae
hyunho.lee <mizzking75@gmail.com>
2021-03-18 11:41:21 +0900

    Implement SearchViewController

--- BookSearch/Screens/SearchViewController.swift
+++ BookSearch/Screens/SearchViewController.swift
@@ -9,23 +9,95 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    let logoImageView = UIImageView()
+    let booknameTextField = BSTextField()
+    let searchActionButton = BSButton(backgroundColor: .systemPink, title: "검색")
+    
+    var isBooknameEntered: Bool { return !booknameTextField.text!.isEmpty }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
-        title = "검색"
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(logoImageView)
+        view.addSubview(booknameTextField)
+        view.addSubview(searchActionButton)
+        
+        configureLogoImageView()
+        configureTextField()
+        configureSearchActionButton()
+        createDismissKeyboardTapGusture()
+//        title = "검색"
+//        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
     }
-    */
+    
+    
+    func createDismissKeyboardTapGusture() {
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
+    
+    
+    @objc func pushBookListViewController() {
+        guard isBooknameEntered else {
+            print("Error!")
+            return
+        }
+        
+        booknameTextField.resignFirstResponder()
+        
+//        let bookListViewController = BookListViewController()
+//        navigationController?.pushViewController(bookListViewController, animated: true)
+    }
+    
+    
+    private func configureLogoImageView() {
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        logoImageView.image = UIImage(named: "bookSearchLogo")
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    
+    private func configureTextField() {
+        let padding: CGFloat = 50
+        
+        booknameTextField.delegate = self
+        NSLayoutConstraint.activate([
+            booknameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: padding),
+            booknameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            booknameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            booknameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    private func configureSearchActionButton() {
+        searchActionButton.addTarget(self, action: #selector(pushBookListViewController), for: .touchUpInside)
+        
+        let padding: CGFloat = 50
+        NSLayoutConstraint.activate([
+            searchActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
+            searchActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            searchActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            searchActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
 
+
+extension SearchViewController: UITextFieldDelegate {
+    
 }
